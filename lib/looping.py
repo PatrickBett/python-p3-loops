c836a135b61c179ca9f269421f063b06f87ff025
PatrickBett <patrickbett018@gmail.com>
2023-11-25 12:07:23 +0300

    First commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i >= 0:
+        if i == 0:
+            print( "Happy New Year!")
+        else:
+            print(i)
+        i-=1
+happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
