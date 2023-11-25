0eed82a0245c603fda4758f70244aea10a569b15
PatrickBett <patrickbett018@gmail.com>
2023-11-25 12:12:35 +0300

    square integers

--- lib/looping.py
+++ lib/looping.py
@@ -8,11 +8,13 @@ def happy_new_year():
         else:
             print(i)
         i-=1
-happy_new_year()
+#happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    for i in int_list:
+        print(i**2)
+square_integers([1,2,3,4,5,6,7,8,9,10])
 
 def fizzbuzz():
     # code goes here!
